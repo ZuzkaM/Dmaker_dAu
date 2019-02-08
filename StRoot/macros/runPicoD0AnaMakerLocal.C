e9c183f55ae34148aab9b9975a4b72d778f73088
ZuzkaM <zuzana.moravcova@fjfi.cvut.cz>
2019-02-08 10:10:51 +0100

    loc

--- StRoot/macros/runPicoD0AnaMakerLocal.C
+++ StRoot/macros/runPicoD0AnaMakerLocal.C
@@ -89,8 +89,8 @@ void runPicoD0AnaMakerLocal(
 
 
   StPicoDstMaker* picoDstMaker = new StPicoDstMaker(static_cast<StPicoDstMaker::PicoIoMode>(StPicoDstMaker::IoRead), inputFile, "picoDstMaker");
-  StPicoD0AnaMaker* PicoD0AnaMaker = new StPicoD0AnaMaker("picoD0AnaMaker", picoDstMaker, outputFile);
-  PicoD0AnaMaker->setHFBaseCuts(hfCuts);
+  //StPicoD0AnaMaker* PicoD0AnaMaker = new StPicoD0AnaMaker("picoD0AnaMaker", picoDstMaker, outputFile);
+  //PicoD0AnaMaker->setHFBaseCuts(hfCuts);
 
 //  StPicoMixedEventMaker* picoMixedEventMaker = new StPicoMixedEventMaker("picoMixedEventMaker", picoDstMaker, hfCuts, outputFile);
 //  picoMixedEventMaker->setBufferSize(3);
