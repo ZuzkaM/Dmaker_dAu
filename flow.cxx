982981cad1a46f44a065be05936e32988cf320df
ZuzkaM <zuzana.moravcova@fjfi.cvut.cz>
2019-02-19 16:41:05 +0100

    tot vs pure bkg

--- flow.cxx
+++ flow.cxx
@@ -4,6 +4,7 @@
 #include "TFile.h"
 #include "TMath.h"
 #include "TH1.h"
+#include "TLegend.h"
 
 using namespace std;
 
@@ -25,6 +26,12 @@ void flow()
     TProfile *corrD2[2]; //cos/sin D pre rozne pT
     TProfile *ReQ;
     TProfile *ImQ;
+
+    //bkg only
+	TProfile *corrDBKG[2][5];
+	TProfile *dirFlowBKG[5];
+	TProfile *corrD2BKG[2];
+	TProfile *dirFlow2BKG;
     
     //TProfile *c2;
     TH1D *c2; 
@@ -41,6 +48,11 @@ void flow()
     TH1D *d2_all_mult_noC;
     TH1D *v2_all_mult_noC;
 
+	TH1D *d2_all_mult_noC_BKG;
+	TH1D *v2_all_mult_noC_BKG;
+
+	TH1D *v2_D0;
+
     //loading TProfiles
  	TString names[4] = {"cos_B", "cos_F", "sin_B", "sin_F"}; //backward and forward samples
   	float multBin[6] = {0,7,12,16,22,100};
@@ -55,19 +67,23 @@ void flow()
   	TString multBinNames[6] = {"0","7","12","16","22","100"};
   	for(int m = 0; m < 5; m++)
   	{
-	  	TString aa = "cosD_" + multBinNames[m] + "_" + multBinNames[m+1];
-	  	corrD[0][m] = (TProfile *)file->Get(aa.Data());
-	  	aa = "sinD_" + multBinNames[m] + "_" + multBinNames[m+1];
-	  	corrD[1][m] = (TProfile *)file->Get(aa.Data());
-	  	aa = "dirFlow_" + multBinNames[m] + "_" + multBinNames[m+1];
-	  	dirFlow[m] = (TProfile *)file->Get(aa.Data());
-  	}
+	  	corrD[0][m] = (TProfile *)file->Get(Form("cosD_%.0f_%.0f", multBin[m], multBin[m+1]));
+		corrDBKG[0][m] = (TProfile *)file->Get(Form("cosD_%.0f_%.0f_BKG", multBin[m], multBin[m+1]));
+		corrD[1][m] = (TProfile *)file->Get(Form("sinD_%.0f_%.0f", multBin[m], multBin[m+1]));
+		corrDBKG[1][m] = (TProfile *)file->Get(Form("sinD_%.0f_%.0f_BKG", multBin[m], multBin[m+1]));
+		dirFlow[m] = (TProfile *)file->Get(Form("dirFlow_%.0f_%.0f", multBin[m], multBin[m+1]));
+		dirFlowBKG[m] = (TProfile *)file->Get(Form("dirFlow_%.0f_%.0f_BKG", multBin[m], multBin[m+1]));
+	}
   	refFlow = (TProfile *)file->Get("refFlow");
 
   	corrD2[0] = (TProfile *)file->Get("cosD_no_mult");
   	corrD2[1] = (TProfile *)file->Get("sinD_no_mult");
 	dirFlow2 = (TProfile *)file->Get("dirFlow_no_mult");
   	refFlow2 = (TProfile *)file->Get("refFlow_no_mult");
+
+	corrD2BKG[0] = (TProfile *)file->Get("cosD_no_mult_BKG");
+	corrD2BKG[1] = (TProfile *)file->Get("sinD_no_mult_BKG");
+	dirFlow2BKG = (TProfile *)file->Get("dirFlow_no_mult_BKG");
 
 
     ReQ = (TProfile *)file->Get("ReQ");
@@ -172,11 +188,26 @@ void flow()
 	d2_all_mult_noC = new TH1D("d2_all_mult_noC", "d_2{2}", 3, momBins);
 	v2_all_mult_noC = new TH1D("v2_all_mult_noC", "v_{2};p_{T};v_{2}", 3, momBins);
 
+	d2_all_mult_noC_BKG = new TH1D("d2_all_mult_noC_BKG", "d_2{2}", 3, momBins);
+	v2_all_mult_noC_BKG = new TH1D("v2_all_mult_noC_BKG", "v_{2};p_{T};v_{2}", 3, momBins);
+
+	v2_D0 = new TH1D("v2_D0", "v_{2} of D0 meson;p_{T};v_{2}", 3, momBins);
+
+
 	cumulant = cum_noC->GetBinContent(1);
 	cumEr = cum_noC->GetBinError(1);
 
 	double dirEr,dirF;
-	
+
+	/*
+	float Nsig = 1;
+	float Nbkg = 2;
+
+	float fSig = Nsig/(Nsig + Nbkg);
+	float fBkg = Nbkg/(Nsig + Nbkg);
+	*/
+
+
 	for(int j = 1; j < 4; j++)
 			{
 				d = dirFlow2->GetBinContent(j);
@@ -188,6 +219,9 @@ void flow()
 				d2_all_mult->SetBinError(j, dirFlow2->GetBinError(j));
 				d2_all_mult_noC->SetBinContent(j, d);
 				d2_all_mult_noC->SetBinError(j, dirFlow2->GetBinError(j));
+
+				d2_all_mult_noC_BKG->SetBinContent(j, dirFlow2BKG->GetBinContent(j));
+				d2_all_mult_noC_BKG->SetBinError(j, dirFlow2BKG->GetBinError(j));
 				/*
 				d2_all_mult->SetBinError(j,  TMath::Sqrt( dirFlow2->GetBinError(j)*dirFlow2->GetBinError(j) + corrD2[0]->GetBinContent(j)*corrD2[0]->GetBinContent(j)*qVec2[0]->GetBinError(1)*qVec2[0]->GetBinError(1) + qVec2[0]->GetBinContent(1)*qVec2[0]->GetBinContent(1)*corrD2[0]->GetBinError(j)*corrD2[0]->GetBinError(j) + corrD2[1]->GetBinContent(j)*corrD2[1]->GetBinContent(j)*qVec2[2]->GetBinError(1)*qVec2[2]->GetBinError(1) + qVec2[2]->GetBinContent(1)*qVec2[2]->GetBinContent(1)*corrD2[1]->GetBinError(j)*corrD2[1]->GetBinError(j) ) );
 				printf("fancy error %f \n", d2_all_mult->GetBinError(j));
@@ -206,14 +240,38 @@ void flow()
 				v2_all_mult_noC->SetEntries(dirFlow2->GetEntries());
 				v2_all_mult_noC->SetBinContent(j,(d2_all_mult_noC->GetBinContent(j))/(cum_noC->GetBinContent(1)));
 
-                dirEr = d2_all_mult->GetBinError(j);
-                dirF = d2_all_mult->GetBinContent(j);
+				v2_all_mult_noC_BKG->SetEntries(dirFlow2BKG->GetEntries());
+				v2_all_mult_noC_BKG->SetBinContent(j,(d2_all_mult_noC_BKG->GetBinContent(j))/(cum_noC->GetBinContent(1)));
+
+                dirEr = d2_all_mult_noC->GetBinError(j);
+                dirF = d2_all_mult_noC->GetBinContent(j);
 
 				error = TMath::Sqrt( (dirEr*dirEr)/(cumulant*cumulant) + (dirF*dirF*cumEr*cumEr)/(cumulant*cumulant*cumulant*cumulant) );
 				v2_all_mult_noC->SetBinError(j,error);
+
+				dirEr = d2_all_mult_noC_BKG->GetBinError(j);
+				dirF = d2_all_mult_noC_BKG->GetBinContent(j);
+
+				error = TMath::Sqrt( (dirEr*dirEr)/(cumulant*cumulant) + (dirF*dirF*cumEr*cumEr)/(cumulant*cumulant*cumulant*cumulant) );
+				v2_all_mult_noC_BKG->SetBinError(j,error);
+
+				//cout << j << "    value      " << (v2_all_mult_noC->GetBinContent(j) - fBkg*(v2_all_mult_noC_BKG->GetBinContent(j)))/fSig << endl;
+				//v2_D0->SetBinContent(j, (v2_all_mult_noC->GetBinContent(j) - fBkg*v2_all_mult_noC_BKG->GetBinContent(j))/fSig);
 			}
 
-	v2_all_mult_noC->Draw();
+	//lame pic is coming
+	TLegend *legend = new TLegend(0.1,0.7,0.48,0.9);
+	legend->SetHeader("v2 D0 analysis","C");
+
+	v2_all_mult_noC_BKG->Draw();
+	v2_all_mult_noC->SetLineColor(kRed);
+	v2_all_mult_noC->Draw("same");
+
+	legend->AddEntry(v2_all_mult_noC_BKG,"Pure bkg (wrong sign)","f");
+	legend->AddEntry(v2_all_mult_noC,"Total (right sign)","f");
+	legend->Draw("same");
+
+	//v2_D0->Draw();
 
 	TFile *output = new TFile("v2_output.root", "recreate");
 	c2->Write();
@@ -228,6 +286,9 @@ void flow()
 	//d2_all_mult->Write();
 	v2_all_mult_noC->Write();
 	d2_all_mult_noC->Write();
+
+	v2_all_mult_noC_BKG->Write();
+	d2_all_mult_noC_BKG->Write();
 
 	//v2_all_mult->Draw();
 	//v2_all_mult_noC->SetLineColor(kRed);
