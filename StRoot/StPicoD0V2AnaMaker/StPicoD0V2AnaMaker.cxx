05d741ab02587440d11c6dda0298ad7b5628ccbe
ZuzkaM <zuzana.moravcova@fjfi.cvut.cz>
2019-03-19 12:39:52 -0400

    Fixing SV

--- StRoot/StPicoD0V2AnaMaker/StPicoD0V2AnaMaker.cxx
+++ StRoot/StPicoD0V2AnaMaker/StPicoD0V2AnaMaker.cxx
@@ -152,6 +152,7 @@ std::vector<int> StPicoD0V2AnaMaker::createCandidates() {
 
                 if (true) {
                     float valueMVA = reader[pTbin]->EvaluateMVA("BDT method");
+                    cout << "Hodnota BDT response " << valueMVA << endl;
                     if(valueMVA < bdtCuts[pTbin]) continue;
 
                     if(charge == 0) mass[pTbin]->Fill( pair->m() );
